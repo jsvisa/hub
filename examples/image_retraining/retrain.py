813b82785011385f7b81e7b9d4150992cd47383a
Delweng Zheng <zwb@caicloud.io>
2018-05-15 18:19:41 +0800

    feat(*): adjust for my useage

--- examples/image_retraining/retrain.py
+++ examples/image_retraining/retrain.py
@@ -166,22 +166,24 @@ def create_image_lists(image_dir, testing_percentage, validation_percentage):
     tf.logging.error("Image directory '" + image_dir + "' not found.")
     return None
   result = collections.OrderedDict()
-  sub_dirs = sorted(x[0] for x in tf.gfile.Walk(image_dir))
+  sub_dirs = sorted(x for x in tf.gfile.Walk(image_dir))
   # The root directory comes first, so skip it.
   is_root_dir = True
   for sub_dir in sub_dirs:
     if is_root_dir:
       is_root_dir = False
       continue
-    extensions = ['jpg', 'jpeg', 'JPG', 'JPEG']
+    if sub_dir[1] != []:
+      continue
+    extensions = ['.jpg', '.jpeg', '.JPG', '.JPEG']
     file_list = []
-    dir_name = os.path.basename(sub_dir)
+    dir_name = os.path.basename(sub_dir[0])
     if dir_name == image_dir:
       continue
     tf.logging.info("Looking for images in '" + dir_name + "'")
-    for extension in extensions:
-      file_glob = os.path.join(image_dir, dir_name, '*.' + extension)
-      file_list.extend(tf.gfile.Glob(file_glob))
+    for f in sub_dir[2]:
+      if os.path.splitext(f)[1] in extensions:
+        file_list.append(os.path.join(image_dir, dir_name, f))
     if not file_list:
       tf.logging.warning('No files found')
       continue
@@ -995,6 +997,8 @@ def main(_):
   # Prepare necessary directories that can be used during training
   prepare_file_system()
 
+  tf.load_file_system_library("/root/caicloud/tensorflow/bazel-bin/tensorflow/core/platform/vulture/vulture_file_system.so")
+
   # Look at the folder structure, and create lists of all the images.
   image_lists = create_image_lists(FLAGS.image_dir, FLAGS.testing_percentage,
                                    FLAGS.validation_percentage)
@@ -1318,7 +1322,7 @@ if __name__ == '__main__':
       '--tfhub_module',
       type=str,
       default=(
-          'https://tfhub.dev/google/imagenet/inception_v3/feature_vector/1'),
+          'http://tfhub.dev/google/imagenet/inception_v3/feature_vector/1'),
       help="""\
       Which TensorFlow Hub module to use.
       See https://github.com/tensorflow/hub/blob/master/docs/modules/image.md
